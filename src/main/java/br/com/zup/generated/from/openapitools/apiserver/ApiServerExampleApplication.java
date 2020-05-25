3f26b7719bec93ca0115869d9753548c14fb2bb2
Hader <hader.araujo@gmail.com>
2020-05-25 12:15:03 -0300

    initial commit

---
+++ src/main/java/br/com/zup/generated/from/openapitools/apiserver/ApiServerExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.generated.from.openapitools.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerExampleApplication.class, args);
+	}
+
+}
